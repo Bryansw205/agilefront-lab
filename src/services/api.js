2b273fead25758e55cfa954021c3c5008379af5f
Bryan Mariños <bryanmg.020@gmail.com>
2025-11-28 13:06:58 -0500

    www

--- src/services/api.js
+++ src/services/api.js
@@ -2,16 +2,18 @@ import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-console.log('API URL:', API_URL); // Para debug
+console.log('API URL:', API_URL);
 
 const api = axios.create({
   baseURL: API_URL,
+  withCredentials: true
 });
 
 export default api;
 
+// --- LOGIN ---
 export const login = async (usuario, contraseña) => {
-  const response = await fetch(`${API_URL}/login`, {
+  const response = await fetch(`${API_URL}/api/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     credentials: 'include',
@@ -20,15 +22,17 @@ export const login = async (usuario, contraseña) => {
   return response.json();
 };
 
+// --- VERIFICAR SESION ---
 export const verificarSesion = async () => {
-  const response = await fetch(`${API_URL}/verificar-sesion`, {
+  const response = await fetch(`${API_URL}/api/verificar-sesion`, {
     credentials: 'include'
   });
   return response.json();
 };
 
+// --- LOGOUT ---
 export const logout = async () => {
-  const response = await fetch(`${API_URL}/logout`, {
+  const response = await fetch(`${API_URL}/api/logout`, {
     method: 'POST',
     credentials: 'include'
   });
